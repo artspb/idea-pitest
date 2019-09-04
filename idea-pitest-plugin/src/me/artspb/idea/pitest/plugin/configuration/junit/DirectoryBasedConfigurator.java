e911a198fdc1d7fd082d39278191c7dfdcbd58e8
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-09-04 11:06:19 +0200

    access PSI in read action

--- idea-pitest-plugin/src/me/artspb/idea/pitest/plugin/configuration/junit/DirectoryBasedConfigurator.java
+++ idea-pitest-plugin/src/me/artspb/idea/pitest/plugin/configuration/junit/DirectoryBasedConfigurator.java
@@ -1,6 +1,8 @@
 package me.artspb.idea.pitest.plugin.configuration.junit;
 
 import com.intellij.execution.junit.JUnitConfiguration;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -19,14 +21,19 @@ import static org.apache.commons.lang.StringUtils.isEmpty;
 class DirectoryBasedConfigurator extends Configurator {
 
     @Override
-    public void configure(JUnitConfiguration jUnitConfiguration, PitestConfiguration configuration) {
+    public void configure(final JUnitConfiguration jUnitConfiguration, PitestConfiguration configuration) {
         JUnitConfiguration.Data data = jUnitConfiguration.getPersistentData();
         String targetTests = "";
         String targetClasses = "";
         String dirName = data.getDirName();
-        VirtualFile dir = LocalFileSystem.getInstance().findFileByPath(FileUtil.toSystemIndependentName(dirName));
+        final VirtualFile dir = LocalFileSystem.getInstance().findFileByPath(FileUtil.toSystemIndependentName(dirName));
         if (dir != null) {
-            PsiDirectory directory = PsiManager.getInstance(jUnitConfiguration.getProject()).findDirectory(dir);
+            PsiDirectory directory = ApplicationManager.getApplication().runReadAction(new Computable<PsiDirectory>() {
+                @Override
+                public PsiDirectory compute() {
+                    return PsiManager.getInstance(jUnitConfiguration.getProject()).findDirectory(dir);
+                }
+            });
             if (directory != null) {
                 PsiPackage psiPackage = JavaDirectoryService.getInstance().getPackage(directory);
                 if (psiPackage != null) {
